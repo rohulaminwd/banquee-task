0530b900b11e1e40d97dd42b0f9a85e5899f1130
rohulaminwd <rohulaminwd@gmail.com>
2022-09-28 22:00:15 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+	
+  // Accordion js code
+  var accItem = document.getElementsByClassName('accordionItem');
+  var accHD = document.getElementsByClassName('accordion-title');
+  for (i = 0; i < accHD.length; i++) {
+      accHD[i].addEventListener('click', toggleItem, false);
+  }
+  function toggleItem() {
+      var itemClass = this.parentNode.className;
+      for (i = 0; i < accItem.length; i++) {
+          accItem[i].className = 'accordionItem close';
+      }
+      if (itemClass == 'accordionItem close') {
+          this.parentNode.className = 'accordionItem open';
+      }
+  }
